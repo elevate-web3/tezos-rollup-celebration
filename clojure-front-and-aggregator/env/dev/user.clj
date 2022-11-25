2877be2132c996ce7ba8800bac71c2d4b41dc39c
Damien Ragoucy <dam@ramdam.io>
2022-11-25 19:37:23 +0100

    Set basic Clojure code that reads a socket

--- clojure-front-and-aggregator/env/dev/user.clj
+++ clojure-front-and-aggregator/env/dev/user.clj
@@ -1,6 +1,11 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :as tools-repl]
+  (:require [aleph.tcp :as tcp]
+            [clj-commons.byte-streams :as bs]
+            [clojure.spec.alpha :as s]
+            [clojure.tools.namespace.repl :as tools-repl]
             [juxt.clip.repl :as clip-repl]
+            [manifold.deferred :as md]
+            [manifold.stream :as ms]
             [rollup.system :as sys]
             [shadow.cljs.devtools.api :as shadow]
             [shadow.cljs.devtools.server :as shadow-server]))
@@ -44,4 +49,21 @@
 
   (refresh-and-restart)
 
+  )
+
+(comment
+  ;; On a terminal, create a file "test.txt" with some text in it.
+  ;; Then run the following command : nc -l -p 1234 < ./test.txt
+  ;; And then run the following expression with a REPL
+  (md/chain
+    (tcp/client {:port 1234 :host "localhost"})
+    (fn [stream]
+      (md/loop []
+        (md/chain
+          (ms/take! stream ::drained)
+          (fn [msg]
+            (if (identical? msg ::drained)
+              (println "End of stream")
+              (do (-> msg (bs/convert String) println)
+                  (md/recur))))))))
   )
