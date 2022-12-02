ee5eb8fd72efbc0611dbfc45147975e6a0030c76
Damien Ragoucy <dam@ramdam.io>
2022-12-02 16:03:18 +0100

    Pass aggregator stream to webserver
    And differenciate site vs API routes through middlewares

--- clojure-front-and-aggregator/src/rollup/server/routes.clj
+++ clojure-front-and-aggregator/src/rollup/server/routes.clj
@@ -1,10 +1,14 @@
 (ns rollup.server.routes
-  (:require [manifold.deferred :as md]
+  (:require [clojure.spec.alpha :as s]
+            [manifold.deferred :as md]
+            [orchestra.core :as _]
             [reitit.ring :as rr]
             [ring.middleware.defaults :as rd]
-            [rollup.server.handlers.public :as public-h]))
+            [rollup.server.aggregator :as aggregator]
+            [rollup.server.handlers.public :as public-h]
+            [rollup.server.util :as u]))
 
-(defn wrap-ring-middleware
+(defn- wrap-ring-middleware
   "Wraps an async capable ring middleware and exposes an Aleph-compliant one.
    This is useful to wrap ring defaults for example.
    (require '[ring.middleware.defaults :as rd])
@@ -22,6 +26,13 @@
           (handler' request #(md/success! response %) #(md/error! response %))
           response)))))
 
+(_/defn-spec wrap-aggregator-stream fn?
+  [handler fn?
+   output-stream ::u/stream]
+  (fn assoc-aggregator-stream [req]
+    (-> (assoc req ::aggregator/output-stream output-stream)
+        handler)))
+
 ;; To be able to recompile handlers on the fly on dev
 ;; h for handler
 (defmacro h [handler-sym]
@@ -31,41 +42,58 @@
       `(fn ~(-> handler-sym name symbol) [req#]
          (~handler-sym req#)))))
 
-(defn make-routes [_config]
-  ["" ;; {:middleware [[my-middleware config]]}
-   ["/" {:name :home-page
-         :get (h public-h/home-page)}]
-   ["/data-stream" {:name :data-stream
-                    :get (h public-h/data-stream)}]])
+(def ^:private wrap-defaults-site
+  (as-> {:params    {:urlencoded true
+                     :multipart  true
+                     :nested     true
+                     ;; :keywordize true
+                     }
+         ;; :session   {:flash true
+         ;;             :cookie-attrs {:http-only true, :same-site :strict}}
+         :security  {:anti-forgery   true
+                     :frame-options  :sameorigin
+                     :content-type-options :nosniff}
+         ;; :static    {:resources "public"}
+         :responses {:not-modified-responses true
+                     :absolute-redirects     false
+                     :content-types          true
+                     :default-charset        "utf-8"}} <>
+    (wrap-ring-middleware
+      (fn middleware [handler]
+        (rd/wrap-defaults handler <>)))))
 
-(defn make-ring-reitit-router []
-  (let [ring-config {:params    {:urlencoded true
-                                 :multipart  true
-                                 :nested     true
-                                 ;; :keywordize true
-                                 }
-                     ;; :session   {:flash true
-                     ;;             :cookie-attrs {:http-only true, :same-site :strict}}
-                     :security  {:anti-forgery   true
-                                 :frame-options  :sameorigin
-                                 :content-type-options :nosniff}
-                     :static    {:resources "public"}
-                     :responses {:not-modified-responses true
-                                 :absolute-redirects     false
-                                 :content-types          true
-                                 :default-charset        "utf-8"}}
-        wrap-defaults-mdlw (wrap-ring-middleware
-                             (fn middleware [handler]
-                               (rd/wrap-defaults handler ring-config)))]
-    (-> (make-routes {})
-        rr/router
-        ;; https://github.com/metosin/reitit/blob/master/doc/ring/slash_handler.md
-        (rr/ring-handler
-          (rr/routes
-            (rr/redirect-trailing-slash-handler {:method :strip})
-            (rr/create-default-handler
-              {:not-found (fn not-found [_req]
-                            {:status  404
-                             :headers {"Content-Type" "text/html"}
-                             :body "<h1>404 not found</h1>"})})))
-        wrap-defaults-mdlw)))
+(def ^:private wrap-defaults-api
+  (as-> {:params    {:urlencoded true
+                     ;; :keywordize true
+                     }
+         :responses {:not-modified-responses true
+                     :absolute-redirects     false
+                     :content-types          true
+                     :default-charset        "utf-8"}} <>
+    (wrap-ring-middleware
+      (fn middleware [handler]
+        (rd/wrap-defaults handler <>)))))
+
+(defn make-routes [_config]
+  [["" {:middleware [wrap-defaults-site]}
+    ["/" {:name :home-page
+          :get (h public-h/home-page)}]]
+   ["" {:middleware [wrap-defaults-api]}
+    ["/data-stream" {:name :data-stream
+                     :get (h public-h/data-stream)}]]])
+
+(_/defn-spec make-ring-reitit-router fn?
+  [m (s/keys :req [::aggregator/output-stream])]
+  (-> (make-routes {})
+      rr/router
+      ;; https://github.com/metosin/reitit/blob/master/doc/ring/slash_handler.md
+      (rr/ring-handler
+        (rr/routes
+          (rr/redirect-trailing-slash-handler {:method :strip})
+          (rr/create-resource-handler {:path "/"})
+          (rr/create-default-handler
+            {:not-found (fn not-found [_req]
+                          {:status  404
+                           :headers {"Content-Type" "text/html"}
+                           :body "<h1>404 not found</h1>"})})))
+      (wrap-aggregator-stream (::aggregator/output-stream m))))
--- clojure-front-and-aggregator/src/rollup/server/webserver.clj
+++ clojure-front-and-aggregator/src/rollup/server/webserver.clj
@@ -3,15 +3,17 @@
             [aleph.netty :as netty]
             [clojure.spec.alpha :as s]
             [orchestra.core :as _]
+            [rollup.server.aggregator :as aggregator]
             [rollup.server.routes :as r]))
 
 (s/def ::aleph-server #(satisfies? netty/AlephServer %))
 (s/def ::port integer?)
 
 (_/defn-spec start-webserver! ::aleph-server
-  [m (s/keys :req-un [::port])]
-  (-> (r/make-ring-reitit-router)
-      (http/start-server m)))
+  [m (s/keys :req [::aggregator/output-stream]
+             :req-un [::port])]
+  (-> (r/make-ring-reitit-router m)
+      (http/start-server (select-keys m [:port]))))
 
 (_/defn-spec stop-webserver! any?
   [^java.io.Closeable server ::aleph-server]
