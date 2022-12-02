39098cab403a01b39a65e6e4221257ee86bc12e4
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2022-12-03 00:35:19 +0100

    thomas do less crazy shit

--- collector/src/main.rs
+++ collector/src/main.rs
@@ -1,31 +1,35 @@
-use anyhow::anyhow;
+use clap::{CommandFactory, Parser};
 use notify::{RecursiveMode, Result, Watcher};
-use std::env;
+use std::fs::File;
 use std::io::{Read, Seek, Write};
 use std::net::{TcpListener, TcpStream};
-use std::path::Path;
 
-use std::fs::File;
+/// Simple program to greet a person
+#[derive(Parser)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short = 'f', long)]
+    log_path: std::path::PathBuf,
 
-// arg parsing clap
+    /// Number of times to greet
+    #[arg(short, long)]
+    port: u16,
+}
 
 fn main() -> anyhow::Result<()> {
     //Collect command line arguments
-    let args: Vec<String> = env::args().collect();
-    let log_file = Path::new(&args[1]);
-    let log_file_dir = log_file
-        .parent()
-        .ok_or_else(|| anyhow!("Could not compute parent directory of log file"))?
-        .canonicalize()?;
-    let log_file = log_file_dir.join(
-        log_file
-            .file_name()
-            .ok_or_else(|| anyhow!("Could not extract file name of log file"))?,
-    );
-    let port: i32 = (&args[2]).parse()?;
 
-    //File::create(log_file)?;
-    let mut log_opt = None;
+    let Args { log_path, port } = Args::parse();
+
+    let mut log_file = match File::open(&log_path) {
+        Ok(f) => f,
+        Err(e) => Args::command()
+            .error(
+                clap::error::ErrorKind::InvalidValue,
+                format_args!("Could not open log file: {e}"),
+            )
+            .exit(),
+    };
     let mut pointer = 0;
 
     let mut should_stop = false;
@@ -43,30 +47,23 @@ fn main() -> anyhow::Result<()> {
             }
         }
     });
-    let (notify_tx, notify_rx) = std::sync::mpsc::channel::<(bool, bool)>();
+    let (notify_tx, notify_rx) = std::sync::mpsc::channel::<bool>();
     let notify_tx_clone_for_ctrlc = notify_tx.clone();
     ctrlc::set_handler(move || {
         match listeners_tx_clone_for_ctrlc.send(None) {
             Ok(()) => {}
             Err(_) => println!("Could not send interrupt to listeners channel"),
         }
-        match notify_tx_clone_for_ctrlc.send((true, false)) {
+        match notify_tx_clone_for_ctrlc.send(true) {
             Ok(()) => {}
             Err(_) => println!("Could not send interrupt to stop channel"),
         }
     })
     .expect("Error setting interrupt handler");
 
-    let log_file_clone = log_file.clone();
     let mut watcher = notify::recommended_watcher(move |res: Result<notify::Event>| match res {
-        Ok(event) => {
-            let _ = notify_tx.send((
-                false,
-                matches!(
-                    event.kind,
-                    notify::EventKind::Create(_) | notify::EventKind::Remove(_)
-                ) && event.paths.contains(&log_file_clone),
-            ));
+        Ok(_) => {
+            let _ = notify_tx.send(false);
         }
         Err(e) => println!("watch error: {:?}", e),
     })?;
@@ -78,57 +75,29 @@ fn main() -> anyhow::Result<()> {
         }
         match listeners_rx.recv() {
             Ok(Some(mut socket)) => {
-                let hello = socket.write_all("Hello!\n".as_bytes());
+                //let hello = socket.write_all("Hello!\n".as_bytes());
                 // Automatically select the best implementation for your platform.
                 {
                     // Add a path to be watched. All files and directories at that path and
                     // below will be monitored for changes.
-                    watcher.watch(&log_file_dir, RecursiveMode::NonRecursive)?;
-                    if log_opt.is_none() && !log_file.exists() {
-                        let (new_should_stop, should_reload) =
-                            notify_rx.recv().expect("watcher is not stopped");
-                        should_stop = new_should_stop;
-                        if should_reload {
-                            log_opt = None;
-                            pointer = 0;
-                        }
-                    }
-                    watcher.watch(&log_file, RecursiveMode::NonRecursive)?;
+                    watcher.watch(&log_path, RecursiveMode::NonRecursive)?;
 
                     // Wait for interrupt signal
                     'notify: loop {
-                        if log_opt.is_none() && !log_file.exists() {
-                            let (new_should_stop, should_reload) =
-                                notify_rx.recv().expect("watcher is not stopped");
-                            should_stop = new_should_stop;
-                            if should_reload {
-                                log_opt = None;
-                                pointer = 0;
-                            }
-                        }
-                        while let Ok((new_should_stop, should_reload)) = notify_rx.try_recv() {
+                        while let Ok(new_should_stop) = notify_rx.try_recv() {
                             should_stop |= new_should_stop;
-                            if should_reload {
-                                log_opt = None;
-                                pointer = 0;
-                            }
                         }
                         if should_stop {
                             break;
                         }
 
-                        if log_opt.is_none() {
-                            log_opt = Some(File::open(&log_file)?);
-                        }
-                        let log = log_opt.as_mut().unwrap();
-
                         const DEFAULT_BUF_SIZE: usize = 16 * 1024;
                         let buf: &mut [_] = &mut [0 as u8; DEFAULT_BUF_SIZE];
                         let pos = std::io::SeekFrom::Start(pointer as u64);
-                        log.sync_data()?;
-                        log.seek(pos)?;
+                        log_file.sync_data()?;
+                        log_file.seek(pos)?;
                         loop {
-                            let read_amount = log.read(buf)?;
+                            let read_amount = log_file.read(buf)?;
                             if read_amount == 0 {
                                 break;
                             }
@@ -142,16 +111,10 @@ fn main() -> anyhow::Result<()> {
                             }
                         }
 
-                        let (new_should_stop, should_reload) =
-                            notify_rx.recv().expect("watcher is not stopped");
-                        should_stop = new_should_stop;
-                        if should_reload {
-                            log_opt = None;
-                            pointer = 0;
-                        }
+                        should_stop = notify_rx.recv().expect("watcher is not stopped");
                     }
 
-                    watcher.unwatch(&log_file)?;
+                    watcher.unwatch(&log_path)?;
 
                     if should_stop {
                         break;
