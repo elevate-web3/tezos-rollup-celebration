78eaa0164bd8e5d3e99c0b5ca81a6e9f8214da24
Damien Ragoucy <dam@ramdam.io>
2022-12-07 14:51:32 +0100

    Run 10 collectors in parallel

--- clojure-front-and-aggregator/src/rollup/system.clj
+++ clojure-front-and-aggregator/src/rollup/system.clj
@@ -7,17 +7,17 @@
             [rollup.server.util :as u]))
 
 (defn get-system-config []
-  {:components
-   {::collectors {:start `(collector/start [{::collector/host "localhost"
-                                             ::collector/port 1234}
-                                            #_{::collector/host "localhost"
-                                             ::collector/port 1235}])
-                  :stop `collector/stop}
-    ::aggregator {:start `(aggregator/start
-                            (merge {::aggregator/flush-ms 50}
-                                   (select-keys (clip/ref ::collectors) [::collector/output-stream])))
-                  :stop `aggregator/stop}
-    ::webserver {:start `(webserver/start-webserver!
-                           (merge {:port 9000}
-                                  (select-keys (clip/ref ::aggregator) [::aggregator/output-stream])))
-                 :stop `webserver/stop-webserver!}}})
+  (let [collectors (->> (range 1234 1244)
+                        (mapv #(do {::collector/host "localhost"
+                                    ::collector/port %})))]
+    {:components
+     {::collectors {:start `(collector/start ~collectors)
+                    :stop `collector/stop}
+      ::aggregator {:start `(aggregator/start
+                              (merge {::aggregator/flush-ms 50}
+                                     (select-keys (clip/ref ::collectors) [::collector/output-stream])))
+                    :stop `aggregator/stop}
+      ::webserver {:start `(webserver/start-webserver!
+                             (merge {:port 9000}
+                                    (select-keys (clip/ref ::aggregator) [::aggregator/output-stream])))
+                   :stop `webserver/stop-webserver!}}}))
