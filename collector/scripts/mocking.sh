423a41d74f3748312c48793aa12914be7d67bc82
Yann Regis-Gianas <yann@nomadic-labs.com>
2022-12-14 15:42:21 +0100

    Update Docker image to mock log generation
    Signed-off-by: Yann Regis-Gianas <yann@nomadic-labs.com>

---
+++ collector/scripts/mocking.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#
+# This script assumes that "gen" from
+# git@gitlab.com:yrg/scoru-demo-quick-and-dirty.git is in the PATH.
+#
+
+ROW="$1"
+COL="$2"
+NB_ROWS="$3"
+NB_COLS="$4"
+PORT="$5"
+TPS="$6"
+
+shift 5
+F=$(mktemp)
+gen "$TPS" "$ROW" "$COL" "$NB_ROWS" "$NB_COLS" $* > "$F" &
+echo -n 'Waiting for the log file to be created'
+while [ ! -s "$F" ]; do sleep 1; echo -n '.'; done
+echo 'done'
+echo 'Running collector.'
+collector --log-path $(cat "$F") --port "$PORT"
