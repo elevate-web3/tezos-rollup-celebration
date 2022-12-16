ab7d198d329e3a6bfab476ac026516d1f98928a7
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2022-12-16 10:14:41 +0100

    modify script with new images

--- collector/scripts/test.sh
+++ collector/scripts/test.sh
@@ -8,5 +8,11 @@ fi
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 "$SCRIPT_DIR"/mocking.sh "$1" "$2" 40 25 "$3" "$4" \
-             ../scoru-demo-quick-and-dirty/img/dogami.ppm \
-             ../scoru-demo-quick-and-dirty/img/objkt.ppm
+            "../scoru-demo-quick-and-dirty/img/10. Youves 01.jpg" \
+            "../scoru-demo-quick-and-dirty/img/2. Dogami 02.jpg" \
+            "../scoru-demo-quick-and-dirty/img/3. objkt.jpg" \
+            "../scoru-demo-quick-and-dirty/img/4. Tezotopia.jpg" \
+            "../scoru-demo-quick-and-dirty/img/5. Fx(hash).jpg" \
+            "../scoru-demo-quick-and-dirty/img/6. Neonz 01.jpg" \
+            "../scoru-demo-quick-and-dirty/img/8. QuipuSwap.jpg" \
+            "../scoru-demo-quick-and-dirty/img/9. TEIA.jpg" \
