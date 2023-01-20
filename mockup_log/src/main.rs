d660bb60fa8d41cb322cc0fb0ec8842cbd1f1e1b
Damien Ragoucy <dam@ramdam.io>
2023-01-20 16:41:14 +0100

    WIP

--- mockup_log/src/main.rs
+++ mockup_log/src/main.rs
@@ -13,8 +13,8 @@ use tokio::io::{self, AsyncWriteExt};
 use tokio::time::{self, Instant};
 
 /// Adjust me to change TPS.
-const INTERVAL_MICROS: u64 = 999;
-// const INTERVAL_MICROS: u64 = 2_000_000;
+const INTERVAL_MICROS: u64 = 200;
+// const INTERVAL_MICROS: u64 = 25_000;
 
 #[tokio::main]
 async fn main() -> Result<()> {
